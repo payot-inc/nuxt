645aaf06d5201ccb8b438bc028a91759c89d4f47
payot <payot.developer@gmail.com>
2026-03-13 16:21:18 +0900

    2026년  3월 13일 금요일 16시 21분 18초 KST

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,19 +1,7 @@
 <script setup lang=ts>
-import type { AppProps } from '@nuxt/ui';
-import { ko } from '@nuxt/ui/locale';
 
-
-const config: AppProps = {
-  locale: ko,
-  toaster: {
-    position: 'bottom-center',
-    max: 3,
-  }
-}
 </script>
 
 <template>
-  <UApp v-bind="config">
 
-  </UApp>
 </template>
