c6b3ddba5e022a95f58f256deb8717ef89621cd0
payot <payot.developer@gmail.com>
2026-03-24 15:50:26 +0900

    2026년  3월 24일 화요일 15시 50분 26초 KST

--- app/composables/getOverlay.ts
+++ app/composables/getOverlay.ts
@@ -1,5 +1,13 @@
-export default function getOverlay() {
+import type { ComponentProps } from "#build/types/layouts";
+import type { OverlayOptions } from "@nuxt/ui/runtime/composables/useOverlay.js";
+import type { Component } from "vue";
+
+export default function getOverlay<T extends Component>(
+  comp: T,
+  options: OverlayOptions<ComponentProps<T>> = { destroyOnClose: false },
+) {
   const overlay = useOverlay();
+  const modal = overlay.create<T>(comp, options);
   onBeforeRouteLeave(() => overlay.closeAll());
-  return overlay.create;
+  return modal;
 }
